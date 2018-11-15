f88d55824dc3227303c48ba7f4eb1024af4e12c9
Paula Kuwałek <paula.kuwalek@gmail.com>
2018-11-15 19:06:36 +0100

    add enter key listener

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var inputC = document.querySelector("input");
 var tempF = document.getElementById("tempF");
 var convertButton = document.querySelector("button");
+var switchButton = document.getElementById("switch");
 var converted = 0;
 
 convertButton.addEventListener("click", function(){
@@ -9,4 +10,15 @@ convertButton.addEventListener("click", function(){
         converted = converted.toFixed(2);
     }
     tempF.textContent = converted;
+});
+
+// Enter key listener
+inputC.addEventListener("keyup", function(e){
+    if (e.keyCode === 13) {
+        converted = inputC.valueAsNumber * 1.8 + 32;
+        if(!Number.isInteger(converted)){
+            converted = converted.toFixed(2);
+        }
+        tempF.textContent = converted;
+    }  
 });

803929a7ae8249708a20a2e8ee226fa700bf0fc3
Paula Kuwałek <paula.kuwalek@gmail.com>
2018-11-18 19:42:23 +0100

    add F to C conversion direction plus small code refactor

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-var inputC = document.querySelector("input");
+var inputC = document.getElementById("inputC");
+var inputF = document.getElementById("inputF");
 var tempF = document.getElementById("tempF");
-var convertButton = document.querySelector("button");
+var tempC = document.getElementById("tempC");
+var convertButton = document.getElementById("convertBtn");
 var switchButton = document.getElementById("switch");
+var farToCel = document.getElementById("farToCel");
+var celToFar = document.getElementById("celToFar");
 var converted = 0;
 
-convertButton.addEventListener("click", function(){
-    converted = inputC.valueAsNumber * 1.8 + 32;
-    if(!Number.isInteger(converted)){
-        converted = converted.toFixed(2);
-    }
-    tempF.textContent = converted;
-});
+convertButton.addEventListener("click", convert);
 
 // Enter key listener
-inputC.addEventListener("keyup", function(e){
+window.addEventListener("keyup", function(e){
     if (e.keyCode === 13) {
+        convert();
+    }
+});
+
+function convert(){
+    if(farToCel.className === "hide"){
         converted = inputC.valueAsNumber * 1.8 + 32;
         if(!Number.isInteger(converted)){
             converted = converted.toFixed(2);
         }
         tempF.textContent = converted;
-    }  
-});
+    } else {
+        converted = (inputF.valueAsNumber - 32) * .5556;
+        if(!Number.isInteger(converted)){
+            converted = converted.toFixed(2);
+        }
+        tempC.textContent = converted;
+    }
+}
+
+function toggle() {
+    farToCel.classList.toggle("hide");
+    celToFar.classList.toggle("hide");
+}
